73a33eff2d30d2e2bcc5d8f451852a32883665b4
Yannick Loriot <yannick.loriot@gmail.com>
2016-02-09 10:57:19 +0100

    Adding Carthage supports

---
+++ Example/PetalFramework/PetalFramework.h
@@ -0,0 +1,19 @@
+//
+//  PetalFramework.h
+//  PetalFramework
+//
+//  Created by Yannick LORIOT on 09/02/16.
+//  Copyright © 2016 Yannick Loriot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PetalFramework.
+FOUNDATION_EXPORT double PetalFrameworkVersionNumber;
+
+//! Project version string for PetalFramework.
+FOUNDATION_EXPORT const unsigned char PetalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PetalFramework/PublicHeader.h>
+
+
